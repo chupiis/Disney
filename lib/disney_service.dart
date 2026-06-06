cc993d9b0ad52c6963e35bb063e0565035a8c52c
Marianna <marianna@Mariannas-MacBook-Air.local>
2026-06-06 19:40:10 +0200

    2

--- lib/disney_service.dart
+++ lib/disney_service.dart
@@ -1,23 +1,57 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:hive_ce/hive.dart';
 import 'character.dart';
 
 class DisneyService {
-  static const String baseUrl = 'https://api.disneyapi.dev/character';
+  static const String baseUrl =
+      'https://api.disneyapi.dev/character';
+
+  final Box cacheBox = Hive.box('characters');
 
   Future<List<DisneyCharacter>> fetchCharacters() async {
-    final response = await http.get(Uri.parse(baseUrl));
+    try {
+      final response = await http.get(Uri.parse(baseUrl));
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+
+        await cacheBox.put(
+          'characters',
+          jsonEncode(data['data']),
+        );
+
+        return (data['data'] as List)
+            .map((e) => DisneyCharacter.fromJson(e))
+            .toList();
+      }
+
+      throw Exception();
+    } catch (e) {
+      final cached = cacheBox.get('characters');
+
+      if (cached != null) {
+        final List decoded = jsonDecode(cached);
+
+        return decoded
+            .map((e) => DisneyCharacter.fromJson(e))
+            .toList();
+      }
+
+      rethrow;
+    }
+  }
+
+  Future<DisneyCharacter> fetchCharacterDetails(int id) async {
+    final response =
+    await http.get(Uri.parse('$baseUrl/$id'));
 
     if (response.statusCode == 200) {
-      final data = json.decode(response.body);
+      final data = jsonDecode(response.body);
 
-      List characters = data['data'];
-
-      return characters
-          .map((json) => DisneyCharacter.fromJson(json))
-          .toList();
-    } else {
-      throw Exception('Failed to load characters');
+      return DisneyCharacter.fromJson(data);
     }
+
+    throw Exception('Failed to load character details');
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:hive_ce_flutter/hive_flutter.dart';
 import 'home_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+  await Hive.openBox('characters');
+
   runApp(const DisneyApp());
 }
 
